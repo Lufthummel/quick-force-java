073f1e9cbd176d41940e2c0fae70d6f56f27a29c
Lufthummel <holger.kremmin@gmail.com>
2017-08-31 10:33:18 +0200

    changed query

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -133,7 +133,7 @@ public class Application extends Controller {
         public F.Promise<List<Account>> getAccounts(AuthInfo authInfo) {
             F.Promise<WSResponse> responsePromise = ws.url(authInfo.instanceUrl + "/services/data/v40.0/query/")
                     .setHeader("Authorization", "Bearer " + authInfo.accessToken)
-                    .setQueryParameter("q", "SELECT Id, Name, Type, Industry, Rating FROM Account")
+                    .setQueryParameter("q", "SELECT Id, Name FROM Account")
                     .get();
 
             return responsePromise.flatMap(response -> {
